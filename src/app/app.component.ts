2bc8062689ab6f87f7aa9255fb3eedaeeed9a742
Ding <advancedor96@gmail.com>
2025-08-23 17:01:36 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'D13_fake_commit_ang';
+}
